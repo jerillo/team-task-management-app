df8b1de260c74859b467c7bcbb2337f616df4124
jerillo <jesnineerillo@berkeley.edu>
2020-05-27 16:51:35 -0700

    Setup file structure

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log('Server is listening on port 3000');
+// Environment variables setup
+require('dotenv').config();
+
+// Requiring routes
+const indexRoutes = require('./routes/index');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', indexRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Server is listening on port ${port}`);
 });
